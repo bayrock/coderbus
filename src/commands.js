67dbdbbb67b387192cf0d713c4e6072b7a9e07e1
Bailey Chestnut <bayrock@users.noreply.github.com>
2022-05-15 15:49:49 -0400

    Register application commands globally

--- src/commands.js
+++ src/commands.js
@@ -1,11 +1,11 @@
 
 module.exports = () => {
-    const { REST } = require('@discordjs/rest')
-    const { Routes } = require('discord-api-types/v10')
-    const { SlashCommandBuilder } = require('@discordjs/builders')
-    const credentials = require('./credentials.json')
+        const { REST } = require('@discordjs/rest')
+        const { Routes } = require('discord-api-types/v10')
+        const { SlashCommandBuilder } = require('@discordjs/builders')
+        const credentials = require('./credentials.json')
 
-    const commands = [
+        const commands = [
         new SlashCommandBuilder().setName('help').setDescription('View the command list'),
         new SlashCommandBuilder().setName('repo').setDescription('View the GitHub repo'),
         new SlashCommandBuilder().setName('languages').setDescription('View the supported languages'),
@@ -15,11 +15,15 @@ module.exports = () => {
         new SlashCommandBuilder().setName('fetch').setDescription('Fetch command')
                 .addStringOption(option => option.setName('url').setDescription('Enter an API endpoint to fetch').setRequired(true))
                 .addBooleanOption(option => option.setName('invisible').setDescription('Toggle the visibility of the response').setRequired(false))
-    ].map(command => command.toJSON())
+        ].map(command => command.toJSON())
 
-    const rest = new REST({ version: '10' }).setToken(credentials.token)
+        const rest = new REST({ version: '10' }).setToken(credentials.token)
 
-    rest.put(Routes.applicationGuildCommands(credentials.clientid, credentials.guildid), { body: commands })
-        .then(() => console.log('Successfully registered application commands.'))
-        .catch(console.error)
+        rest.put(Routes.applicationCommands(credentials.clientid), { body: commands })
+                .then(() => console.log('Successfully registered global commands.'))
+                .catch(console.error)
+
+        // rest.put(Routes.applicationGuildCommands(credentials.clientid, credentials.guildid), { body: commands })
+        //         .then(() => console.log('Successfully registered guild commands.'))
+        //         .catch(console.error)
 }
